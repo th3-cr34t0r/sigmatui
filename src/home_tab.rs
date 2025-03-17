4e34b5059cc42ad04c7297eafecb38d7fe11fe0b
ergXned <126957013+ergXned@users.noreply.github.com>
2025-03-17 14:30:58 +0100

    home_tab: updated Top Miners with a table of wallets and corresponding hashrate, updated Pool Hashrate with a chart with demo values

--- src/home_tab.rs
+++ src/home_tab.rs
@@ -1,7 +1,11 @@
 use ratzilla::ratatui::{
     layout::{Alignment, Constraint, Flex, Layout, Rect},
     style::Stylize,
-    widgets::{Block, Borders, Chart, Dataset, List, ListItem, Paragraph},
+    symbols,
+    widgets::{
+        canvas::Label, Axis, Block, BorderType, Borders, Chart, Dataset, GraphType, List, ListItem,
+        Paragraph, Row, Table,
+    },
     Frame,
 };
 
@@ -11,6 +15,7 @@ impl HomeTab {
     pub fn new() -> Self {
         Self {}
     }
+
     ///Render the tab frame
     pub fn render(&mut self, f: &mut Frame, area: Rect) {
         let vertical = Layout::vertical([Constraint::Percentage(50), Constraint::Percentage(50)]);
@@ -26,37 +31,84 @@ impl HomeTab {
         self.pool_hashrate(f, pool_hashrate_area);
 
         self.pool_info(f, bottom_area);
-        // let (widget, area) = self.top_addresses(bottom_left);
-        // f.render_widget(widget, area);
-
-        // let (widget, area) = self.top_addresses(bottom_right);
-        // f.render_widget(widget, area);
     }
 
     ///Provide logic for the top addresses
     fn top_addresses(&mut self, f: &mut Frame, area: Rect) {
-        let widget = List::new(vec![
-            ListItem::new("addr_1"),
-            ListItem::new("addr_2"),
-            ListItem::new("addr_2"),
-        ])
-        .block(Block::bordered().title_top("Top Addresses"));
+        let rows = [
+            Row::new(vec!["address_1", "12.2 Gh/s"]),
+            Row::new(vec!["address_2", "11.5 Gh/s"]),
+            Row::new(vec!["address_3", "hashrate"]),
+            Row::new(vec!["address_4", "hashrate"]),
+            Row::new(vec!["address_5", "hashrate"]),
+            Row::new(vec!["address_6", "hashrate"]),
+        ];
 
-        f.render_widget(widget, area);
+        let widths = [Constraint::Percentage(70), Constraint::Percentage(30)];
+
+        let table = Table::new(rows, widths)
+            .header(Row::new(vec!["Address", "Hashrate"]).on_red())
+            .block(
+                Block::bordered()
+                    .border_type(BorderType::Rounded)
+                    .title_top("Top Miners"),
+            );
+
+        f.render_widget(table, area);
     }
-    fn pool_hashrate(&mut self, f: &mut Frame, area: Rect) {
-        let dataset = vec![(1.5, 16.1), (1.6, 16.6), (1.5, 13.6), (1.4, 18.6)];
 
-        let chart = Chart::new(vec![Dataset::default().data(&dataset)])
-            .block(Block::bordered().title_top("Pool Hashrate"));
+    ///Provide logic for pool hashrate section
+    fn pool_hashrate(&mut self, f: &mut Frame, area: Rect) {
+        let data = vec![
+            (0.0, 16.1),
+            (1.0, 16.6),
+            (2.0, 13.6),
+            (3.0, 18.6),
+            (4.0, 16.1),
+            (5.0, 16.6),
+            (6.0, 13.6),
+            (7.0, 18.6),
+            (8.0, 16.1),
+            (9.0, 16.6),
+            (10.0, 13.6),
+            (11.0, 18.6),
+            (12.0, 16.1),
+        ];
+
+        let dataset = Dataset::default()
+            .graph_type(GraphType::Line)
+            .data(&data)
+            .light_yellow();
+
+        let chart = Chart::new(vec![dataset])
+            .block(
+                Block::bordered()
+                    .border_type(BorderType::Rounded)
+                    .title_top("Pool Hashrate"),
+            )
+            .x_axis(
+                Axis::default()
+                    .bounds([0.0, 12.0])
+                    .labels(["0.0".bold(), "6.0".into(), "12.0".bold()])
+                    .title("Hours"),
+            )
+            .y_axis(
+                Axis::default()
+                    .bounds([13.0, 19.0])
+                    .labels(["13.0".bold(), "15.0".into(), "19.0".bold()])
+                    .title("Gh/s"),
+            );
 
         f.render_widget(chart, area);
     }
-    fn pool_info(&mut self, f: &mut Frame, area: Rect) {
-        let widget = Paragraph::new("Pool Info")
-            .block(Block::bordered())
-            .alignment(Alignment::Center);
 
+    ///Provide logic for pool info section
+    fn pool_info(&mut self, f: &mut Frame, area: Rect) {
+        let widget = Paragraph::new("").block(
+            Block::bordered()
+                .border_set(symbols::border::ROUNDED)
+                .title_top("Pool Info"),
+        );
         f.render_widget(widget, area);
     }
 }
